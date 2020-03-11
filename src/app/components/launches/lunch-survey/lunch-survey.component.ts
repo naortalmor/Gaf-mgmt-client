139951aa997270a379f1bc8750e6ddcefe78d773
Naor Talmor <naortalmornew@gmail.com>
2020-03-11 14:35:25 +0200

    add tabs and split the lunch component into two modes

---
+++ src/app/components/launches/lunch-survey/lunch-survey.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lunch-survey',
+  templateUrl: './lunch-survey.component.html',
+  styleUrls: ['./lunch-survey.component.css']
+})
+export class LunchSurveyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
