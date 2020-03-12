50403d0560ec8c3a434459bb37785311115af196
Lior <42876247+lior21@users.noreply.github.com>
2020-03-12 02:39:12 +0200

    bubbles calls

--- src/app/services/mifgaf.service.ts
+++ src/app/services/mifgaf.service.ts
@@ -5,9 +5,13 @@ import { AppState } from '../store/state';
 import { Person } from '../models/interfaces/person';
 import { INIT_THIS_WEEK_PERSONS } from '../store/this-week-persons/this-week-persons.actions';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class MifgafService {
+
+  infoBubblesObs:Subject<{ title: string, data: string }[]>;
+  private infoBubbles: { title: string, data: string }[];
 
   constructor(private store: Store<AppState>,
     private http: HttpClient) {
@@ -15,6 +19,29 @@ export class MifgafService {
 
   initThisWeekPersons() {
     this.http.get(`${config.serverUrl}/mifgafim/test`)
-    .subscribe((thisWeekPersons:Person[]) => this.store.dispatch(INIT_THIS_WEEK_PERSONS({thisWeekPersons})));
+      .subscribe((thisWeekPersons: Person[]) => this.store.dispatch(INIT_THIS_WEEK_PERSONS({ thisWeekPersons })));
   }
+
+  initInfoBubbles() {
+    this.infoBubbles = [];  
+    this.http.get(`${config.serverUrl}/mifgafim/howLongShouldIWaitToWin`)
+      .subscribe((howlong: string) => this.insertBubble({
+        title: "מתי אני מביא?",
+        data: howlong
+      }));
+      this.http.get(`${config.serverUrl}/mifgafim/whenIWonLastly`)
+      .subscribe((when: string) => this.insertBubble({
+        title: "מתי הבאתי לאחרונה?",
+        data: when
+      }));
+  }
+
+  private insertBubble(bubble):void {
+    let bubbles = [...this.infoBubbles];
+    bubbles.push(bubble);
+    this.infoBubblesObs.next(bubbles);
+    this.infoBubbles = bubbles;
+  }
+
+
 }
