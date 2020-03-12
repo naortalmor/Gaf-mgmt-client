e20483f5c36ee29f481dfe4ece7908097ea42e32
Raz Tamir <raz6tamir@gmail.com>
2020-03-12 18:13:08 +0200

    start firebase hosting

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -16,19 +16,19 @@ export class AdminService {
     this.afs.doc<User[]>(`users/`).valueChanges().subscribe(users => {
 
     });
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
-    const userData: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      emailVerified: user.emailVerified,
-      roles: {
-        guest: true
-      }
-    };
-    return userRef.set(userData, {
-      merge: true
-    });
+    // const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    // // const userData: User = {
+    // //   uid: user.uid,
+    // //   email: user.email,
+    // //   displayName: user.displayName,
+    // //   photoURL: user.photoURL,
+    // //   emailVerified: user.emailVerified,
+    // //   roles: {
+    // //     guest: true
+    // //   }
+    // // };
+    // return userRef.set(userData, {
+    //   merge: true
+    // });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDl37-QU5sm0xb3IYG7VPOxe7aTaL0Zi3I',
+    authDomain: 'gaf-mgmt.firebaseapp.com',
+    databaseURL: 'https://gaf-mgmt.firebaseio.com',
+    projectId: 'gaf-mgmt',
+    storageBucket: 'gaf-mgmt.appspot.com',
+    messagingSenderId: '82n4441709231',
+    appId: '1:824441709231:web:91dcf66f0354a42fa52a5a',
+    measurementId: 'G-FHMKGHHLCQ'
+  }
 };
