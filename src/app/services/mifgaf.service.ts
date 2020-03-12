e46efd8f7b1ca0378bb1a74a080e0d3448351c9f
Lior <42876247+lior21@users.noreply.github.com>
2020-03-12 02:17:20 +0200

    css mifgaf

--- src/app/services/mifgaf.service.ts
+++ src/app/services/mifgaf.service.ts
@@ -5,15 +5,29 @@ import { Person } from '../models/interfaces/person';
 import { INIT_THIS_WEEK_PERSONS } from '../store/this-week-persons/this-week-persons.actions';
 import { HttpClient } from '@angular/common/http';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class MifgafService {
 
-  constructor(private store:Store<AppState>,
-              private http:HttpClient) {
+  constructor(private store: Store<AppState>,
+    private http: HttpClient) {
   }
 
   initThisWeekPersons() {
+    this.store.dispatch(INIT_THIS_WEEK_PERSONS({ thisWeekPersons: [
+      {
+        name: 'תמיר',
+        age: 16
+      },
+      {
+        name: 'תמיר',
+        age: 16
+      },
+      {
+        name: 'תמיר',
+        age: 16
+      }
+    ] as Person[] }));
     this.http.get('http://localhost:1111/mifgafim/test')
-    .subscribe((thisWeekPersons:Person[]) => this.store.dispatch(INIT_THIS_WEEK_PERSONS({thisWeekPersons})));
+      .subscribe((thisWeekPersons: Person[]) => this.store.dispatch(INIT_THIS_WEEK_PERSONS({ thisWeekPersons })));
   }
 }
